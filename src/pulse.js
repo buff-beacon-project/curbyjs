e3be86671840ba2e9549d753b0b704cb56a50ab2
Jasper <well.caffeinated@gmail.com>
2021-08-06 18:32:27 -0600

    wip certificate validation

--- src/pulse.js
+++ src/pulse.js
@@ -1,12 +1,76 @@
 import axios from 'axios'
+import { getTimeStamp } from './util'
+import { pki } from 'node-forge'
 
-const fetch = axios.create({
+const beaconFetch = axios.create({
   baseURL: 'http://71.56.217.42:8000/api/'
   , timeout: 10000
 });
 
+const CERTIFICATES = {}
+
+export function getPulseDigest(pulse){
+  return new ArrayBuffer(512)
+}
+
+async function parseResponseAndValidate(res){
+  const pulse = res.data
+
+  let cert = CERTIFICATES[pulse.certificate_id]
+  if (!cert){
+    cert = pki.certificateFromPem(await fetchCertificate(pulse.certificate_id), true)
+    CERTIFICATES[pulse.certificate_id] = cert
+  }
+
+  const digest = getPulseDigest()
+  if (!cert.verify(digest, pulse.signature)){
+    throw new Error('Invalid pulse signature!')
+  }
+
+  return pulse
+}
+
+export function fetchCertificate(hashId){
+  return beaconFetch(`/certificate/${hashId}`, {
+    responseType: 'text'
+  })
+    .then(res => res.data)
+    .then(text => {
+      return text.replace(
+        /-----BEGIN ([A-Za-z0-9- ]+)-----/,
+        '-----BEGIN CERTIFICATE-----'
+      ).replace(
+        /-----END ([A-Za-z0-9- ]+)-----/,
+        '-----END CERTIFICATE-----'
+      )
+    })
+    .then(text => {
+      console.log(text)
+      return text
+    })
+}
+
+export function fetch(chain, pulse){
+  return beaconFetch(`/chain/${chain}/pulse/${pulse}`).then(parseResponseAndValidate)
+}
+
 export function fetchLast() {
-  return fetch('/last').then(r => r.data)
+  return beaconFetch('/last').then(parseResponseAndValidate)
+}
+
+export function fetchAt(date){
+  const ts = getTimeStamp(date)
+  return beaconFetch(`/at/${ts}`).then(parseResponseAndValidate)
+}
+
+export function fetchAfter(date) {
+  const ts = getTimeStamp(date)
+  return beaconFetch(`/after/${ts}`).then(parseResponseAndValidate)
+}
+
+export function fetchBefore(date) {
+  const ts = getTimeStamp(date)
+  return beaconFetch(`/before/${ts}`).then(parseResponseAndValidate)
 }
 
 export function nextPulseAt() {
--- src/util.js
+++ src/util.js
@@ -1,3 +1,20 @@
+// eslint-disable-next-line max-len
+const isoDateRegExp = /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))/
+
+export function isISODate(str) {
+  return isoDateRegExp.test(str);
+}
+
+export function getTimeStamp(isoStrOrDate){
+  if (typeof date === 'string' && isISODate(isoStrOrDate)) {
+    return isoStrOrDate
+  } else if (isoStrOrDate instanceof Date) {
+    return isoStrOrDate.toISOString()
+  }
+
+  throw new Error('Date must be an ISO compliant datetime string, or a Date instance')
+}
+
 export function hex2buf(input) {
   if (typeof input !== 'string') {
     throw new TypeError('Input must be a string')
